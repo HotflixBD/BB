e8c6226d52a903da6cf918ede6c19840cd9d84e0
HotflixBD <119289128+HotflixBD@users.noreply.github.com>
2025-01-26 13:10:23 +0900

    Update azaxa.js

--- azaxa.js
+++ azaxa.js
@@ -1,19 +1,30 @@
   var i1 = document.querySelector('#t1'); 
-  var l='https://me.webcric.com/frame5.htm';
-  var l='https://me.webcric.com/frame3.htm';
-  var l1='https://me.webcric.com/hindi.htm';
-  var l='https://s.bdixtv24.com/sportstv.php?c=star1in';
-  var l='https://fclembed.online/hembedplayer/csstream1/1/960/540';
-
-  
   var i2 = document.querySelector('#t2'); 
-  var l2='https://fclembed.online/hembedplayer/csstream1/1/960/540';
-  var l='https://me.webcric.com/frame3.htm';
-	
-  
   var i3 = document.querySelector('#t3'); 
-  var l='https://cdn.crichdplays.ru/embed2.php?id=willow';
-  var l='https://tv.durbinlive.pro/api/play3.php?c=https://m118753.protect-cdn.net/live/m4-720p.m3u8';
-  var l='https://s.bdixtv24.com/sportstv.php?c=starhindi';
-  var l3='https://me.webcric.com/frame1.htm';
-	
+  var l1='https://me.webcric.com/frame1.htm';
+  var l2='https://me.webcric.com/frame2.htm';
+  var l3='https://me.webcric.com/frame3.htm';
+  var l4='https://me.webcric.com/frame4.htm';
+  var l5='https://me.webcric.com/frame5.htm';
+  var l6='https://me.webcric.com/frame6.htm';
+  var l7='https://me.webcric.com/frame7.htm';
+  var l8='https://me.webcric.com/frame8.htm';
+  var l9='https://me.webcric.com/frame9.htm';
+  var l10='https://me.webcric.com/frame10.htm';
+
+  var l11='https://me.webcric.com/hindi.htm';
+  var l12='https://me.webcric.com/willow.htm';
+  var l13='https://me.webcric.com/foxcricket.htm';
+  var l14='https://me.webcric.com/criclife1.htm';
+  var l15='https://me.webcric.com/criclife2.htm';
+
+  var l16='https://s.bdixtv24.com/sportstv.php?c=star1in';
+  var l17='https://fclembed.online/hembedplayer/csstream1/1/960/540';
+  var l18='https://fclembed.online/hembedplayer/csstream2/1/960/540';
+  var l19='https://fclembed.online/hembedplayer/csstream3/1/960/540';
+  var l20='https://fclembed.online/hembedplayer/csstream4/1/960/540';
+  var l21='https://fclembed.online/hembedplayer/csstream5/1/960/540';
+  var l22='https://fclembed.online/hembedplayer/csstream6/1/960/540';
+
+  var l23='https://cdn.crichdplays.ru/embed2.php?id=willow';
+  var l24='https://tv.durbinlive.pro/api/play3.php?c=https://m118753.protect-cdn.net/live/m4-720p.m3u8';
