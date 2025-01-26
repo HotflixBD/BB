7bafc1563a99b49fce0079fd6408e9c1aadd3195
HotflixBD <119289128+HotflixBD@users.noreply.github.com>
2025-01-26 12:20:12 +0900

    Create azax.js

---
+++ azax.js
@@ -0,0 +1,19 @@
+	var i1 = document.querySelector('#t1'); 
+  var l='https://me.webcric.com/frame5.htm';
+  var l1='https://me.webcric.com/frame3.htm';
+  var l='https://me.webcric.com/hindi.htm';
+  var l='https://s.bdixtv24.com/sportstv.php?c=star1in';
+  var l='https://fclembed.online/hembedplayer/csstream1/1/960/540';
+	i1.setAttribute('src', l1); 
+  
+  var i2 = document.querySelector('#t2'); 
+  var l2='https://fclembed.online/hembedplayer/csstream1/1/960/540';
+  var l='https://me.webcric.com/frame3.htm';
+	i2.setAttribute('src', l2); 
+  
+  var i3 = document.querySelector('#t3'); 
+  var l='https://cdn.crichdplays.ru/embed2.php?id=willow';
+  var l='https://tv.durbinlive.pro/api/play3.php?c=https://m118753.protect-cdn.net/live/m4-720p.m3u8';
+  var l='https://s.bdixtv24.com/sportstv.php?c=starhindi';
+  var l3='https://me.webcric.com/frame1.htm';
+	i3.setAttribute('src', l3); 
